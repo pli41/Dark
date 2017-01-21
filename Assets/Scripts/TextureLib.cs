dab6da763b38d8956900d18be0d38cfea2f1f75b
LiPeilin <lipeilin@LiPeilins-MBP.wv.cc.cmu.edu>
2017-01-21 14:10:44 -0500

    setup

---
+++ Assets/Scripts/TextureLib.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureLib : MonoBehaviour {
+
+	static Dictionary<string, Sprite> spriteDict;
+
+	public Sprite metalWall;
+	public Sprite metalFloor;
+	public Sprite stoneWall;
+	public Sprite stoneFloor;
+	public Sprite blood;
+	public Sprite water;
+	public Sprite woodDoor;
+
+	// Use this for initialization
+	void Start () {
+		PopulateDict ();
+	}
+
+	void PopulateDict(){
+		spriteDict.Add ("metalWall", metalWall);
+		spriteDict.Add ("metalFloor", metalFloor);
+		spriteDict.Add ("stoneWall", stoneWall);
+		spriteDict.Add ("stoneFloor", stoneFloor);
+		spriteDict.Add ("woodDoor", woodDoor);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static Sprite Find(string name){
+		return spriteDict [name];
+	}
+}
---
+++ Assets/Scripts/WaveDetectionSystem.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveDetectionSystem : MonoBehaviour {
+
+	public LayerMask hitLayer;
+	public float detectionRange;
+	public bool detectionReady;
+
+	Transform camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = transform.Find ("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator WaitForDetection(){
+		while(true){
+			if(detectionReady){
+				if(Input.GetKeyDown(KeyCode.F)){
+					StartDetection ();
+				}
+			}
+
+			yield return null;
+		}
+	}
+
+	void StartDetection(){
+		RaycastHit hit;
+
+		if(Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, detectionRange, hitLayer)){
+			float distance = Vector3.Distance (camera.position, hit.point);
+
+		}
+	}
+
+
+}
